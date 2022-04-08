c09c25912b7eeed592832d5cfb779e40a01bafbc
Oreoluwa200 <94811796+Oreoluwa200@users.noreply.github.com>
2022-04-08 04:40:06 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #!/bin/bash
-#include<stdio.h>
+
+#include <stdio.h>
+/**
+ * main - The containing function of this programme
+ * Return: this gives 0 if sucessful
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n"):
-return (0):
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
